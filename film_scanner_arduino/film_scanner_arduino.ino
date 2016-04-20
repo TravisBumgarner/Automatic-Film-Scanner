5eb840612e35b28545d5aadb2f604052e2a1a213
Travis Bumgarner <travis.bumgarner@gmail.com>
2016-04-19 20:23:18 -0400

    V0.3
    - Modifying how information is printed on LCD screen

--- film_scanner_arduino/film_scanner_arduino.ino
+++ film_scanner_arduino/film_scanner_arduino.ino
@@ -2,21 +2,23 @@
 
 // Libraries & Intialization
 /*
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 2
- * LCD D5 pin to digital pin 3
- * LCD D6 pin to digital pin 4
- * LCD D7 pin to digital pin 5
- */
+   LCD RS pin to digital pin 12
+   LCD Enable pin to digital pin 11
+   LCD D4 pin to digital pin 2
+   LCD D5 pin to digital pin 3
+   LCD D6 pin to digital pin 4
+   LCD D7 pin to digital pin 5
+*/
 #include <LiquidCrystal.h>
-LiquidCrystal lcd(12, 11, 2,3,4,5);
+LiquidCrystal lcd(12, 11, 2, 3, 4, 5);
 
 int continueButton = 13;
+int calibrateStepperMotorButton = 7;
 int continueButtonValue;
+int numberOfPhotosToScan;
 
-int stmMove = 9
-int stmDir = 8
+int stmMovePin = 9;
+int stmDirPin = 8;
 
 /*********************************************************/
 /***************  lcdClearLine()  ************************/
@@ -24,19 +26,19 @@ int stmDir = 8
 
 void lcdClearLine()
 {
-    lcd.setCursor(0,1);
-    lcd.print("                                ");
-    lcd.setCursor(0,0);
-    lcd.print("                                ");
-    lcd.setCursor(0,0);
-} 
+  lcd.setCursor(0, 1);
+  lcd.print("                                ");
+  lcd.setCursor(0, 0);
+  lcd.print("                                ");
+  lcd.setCursor(0, 0);
+}
 
 /*********************************************************/
 /***************  readInputButtons()  ********************/
 /*********************************************************/
 /*Reads the values of all the physical buttons in circuit*/
-void readInputButtons(){
-    continueButtonValue = digitalRead(continueButton);
+void readInputButtons() {
+  continueButtonValue = digitalRead(continueButton);
 }
 
 /*********************************************************/
@@ -44,16 +46,66 @@ void readInputButtons(){
 /*********************************************************/
 /*Checks that each part of the system is running before  */
 /* Continuing running                                    */
-void systemStartupChecks(String stringToCheck, String stringToCheckLine2 = ""){
-  while(continueButtonValue != 1){
-    readInputButtons(); 
+void systemStartupChecks(String stringToCheck, String stringToCheckLine2 = "") {
+  while (continueButtonValue != 1) {
+    readInputButtons();
     lcdClearLine();
     lcd.print(stringToCheck);
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.print(stringToCheckLine2);
   }
   delay(500);
 }
+
+/*********************************************************/
+/***********************  stm()  *************************/
+/*********************************************************/
+/*Moves stepper motor in a specific direction with setDirection*/
+/*Moves stepper motor for a setDistance /*                                  */
+void stm(boolean setDirection, int setDistance) {
+  Serial.println(String(digitalRead(stmDirPin)) + " " + String(setDistance));
+  digitalWrite(stmDirPin, setDirection);
+  for (int i = 0; i < setDistance; i++) {
+    digitalWrite(stmMovePin, HIGH);
+    delay(1);
+    digitalWrite(stmMovePin, LOW);
+    delay(1);
+  }
+}
+
+/*********************************************************/
+/************  calibrateStepperMotor()  ******************/
+/*********************************************************/
+/*Simple button that moves stepper motor to where you want it to go*/
+void calibrateStepperMotor() {
+  digitalWrite(stmMovePin, HIGH);
+  delay(5);
+  digitalWrite(stmMovePin, LOW);
+  delay(5);
+}
+
+/*********************************************************/
+/***************  numberOfPhotos()  **********************/
+/*********************************************************/
+/*Declare number of negatives to scan */
+void numberOfPhotos(){
+    while(continueButtonValue != HIGH){
+        readInputButtons();
+        lcd.setCursor(0,0);
+        lcd.print("Negatives QTY:");
+        if(buttonSetValue == HIGH){ /******************************** Declare buttonSetValue *************/
+            return;
+        }
+        if(buttonUp == HIGH) {numberOfPhotosToScan++;}
+        else if (buttonDown == HIGH)    {numberOfPhotosToScan--;}    
+        }
+        lcd.setCursor(0,1);
+        lcd.print(numberOfPhotosToScan); //Change to variables
+        delay(50);
+    }
+}
+
+
 
 /*********************************************************/
 /******************  setup()  ****************************/
@@ -61,23 +113,31 @@ void systemStartupChecks(String stringToCheck, String stringToCheckLine2 = ""){
 
 void setup() {
   lcd.begin(16, 2);
-  pinMode(continueButton,INPUT);
+  pinMode(continueButton, INPUT);
+  pinMode(calibrateStepperMotorButton, INPUT);
   Serial.begin(9600);
-  pinMode(stmDir, OUTPUT);     
-  pinMode(stmMove, OUTPUT);
-  digitalWrite(stmDir, LOW);
-  digitalWrite(stmMove, LOW);
-
+  pinMode(stmDirPin, OUTPUT);
+  pinMode(stmMovePin, OUTPUT);
+  digitalWrite(stmDirPin, LOW);
+  digitalWrite(stmMovePin, LOW);
+  Serial.begin(9600);
 }
 
 /*********************************************************/
 /******************  loop()  *****************************/
 /*********************************************************/
 void loop() {
-  systemStartupChecks("Load Film and  ","Tighten");
+  systemStartupChecks("Load Film and  ", "Tighten");
   readInputButtons();
-  systemStartupChecks("Check Camera:RAW","M Mode, Level it");
+  systemStartupChecks("Check Camera:RAW", "M Mode, Level it");
   readInputButtons();
-  systemStartupChecks("Take Test Shot");
+  systemStartupChecks("Adj Stepper", "Take Test Shot");
   readInputButtons();
+  while (continueButtonValue != 1) {
+    if (digitalRead(calibrateStepperMotorButton) == HIGH) {
+      calibrateStepperMotor();
+    }
+  }
+
+
 }
